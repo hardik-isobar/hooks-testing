71012614ffedd1a412c78a22d7db18e65801857d
Hardik Shah <62693225+hardik-isobar@users.noreply.github.com>
2021-05-18 13:04:11 +0530

    iphone devices scroll fix with body-scroll-lock

--- src/Modal.js
+++ src/Modal.js
@@ -1,10 +1,26 @@
-import { useLockBodyScroll } from "react-use";
+import { useEffect } from "react";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
 
 export default function Modal({ closeModal }) {
-  useLockBodyScroll();
+  const close = () => {
+    enableBodyScroll(document.querySelector("body"));
+    closeModal();
+  };
+
+  useEffect(() => {
+    disableBodyScroll(document.querySelector("body"));
+    return () => {
+      clearAllBodyScrollLocks();
+    };
+  }, []);
+
   return (
     <div className="modal-overlay">
-      <div className="modal-close" onClick={closeModal}>
+      <div className="modal-close" onClick={close}>
         x
       </div>
       <div className="modal-body">
@@ -80,6 +96,45 @@ export default function Modal({ closeModal }) {
           Aldus PageMaker including versions of Lorem Ipsum.
         </div>
 
+        <div>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+
+        <div>Is modal scrolling</div>
+        <div>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+
+        <div>Is modal scrolling</div>
+        <div>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+
+        <div>Is modal scrolling</div>
         <div>
           What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
           and typesetting industry. Lorem Ipsum has been the industry's standard
